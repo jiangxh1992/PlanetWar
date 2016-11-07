3897d417e3a6f5e8bdfe3aedf8d49cc6d6bf27b4
Xinhou Jiang <xinhoujiang@Xinhous-MacBook-Pro.local>
2016-11-07 23:42:52 +0800

    单机版球球大作战单机版初始工程 v1.0

---
+++ PlanetWar/Classes/BaseBall.cpp
@@ -0,0 +1,9 @@
+//
+//  BaseBall.cpp
+//  PlanetWar
+//
+//  Created by Xinhou Jiang on 7/11/16.
+//
+//
+
+#include "BaseBall.h"
---
+++ PlanetWar/Classes/BaseBall.h
@@ -0,0 +1,24 @@
+//
+//  BaseBall.h
+//  PlanetWar
+//
+//  Created by Xinhou Jiang on 7/11/16.
+//
+//
+
+#ifndef __PlanetWar__BaseBall__ 
+#define __PlanetWar__BaseBall__
+
+#include <stdio.h>
+#include "cocos2d.h"
+
+class BaseBall : public cocos2d::Sprite
+{
+public:
+    
+    // implement the "static create()" method manually
+    CREATE_FUNC(BaseBall);
+};
+
+
+#endif /* defined(__PlanetWar__BaseBall__) */
